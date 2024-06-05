8223b5aa8d5fb57d7d7ee64e68e4d2c34ac6efd9
root <root@debian.MasterClasse.ipssi>
2024-06-05 18:50:52 +0200

    Bon fonctionnement de la partie ajout d'un fichier csv en fonction des dates,implémentation de la page affichant la liste des fichiers csv et permettant le telechargement

--- csvdownload.php
+++ csvdownload.php
@@ -1,5 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'inscription</title>
+    <link rel="stylesheet" href="./styles/styles.css">
+</head>
+<body>
+    <table>
+<tr>
+    
+    <h1>Liste des Fichiers CSV</h1>
+    <?php
+    $dossier = scandir("./backend/csvFiles");
+    foreach($dossier as $fichier){
+    	echo "<a href='./backend/csvFiles/$fichier'csv' download >$fichier<br></a>";
+    }
+    ?>
+</tr>
+
+</table>
+   
+    
+</body>
+</html>
 
 
 
-?>
